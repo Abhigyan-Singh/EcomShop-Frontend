9d151b482b06876802247f2d92b81da3b5dfe01a
n.vas <naadir.vasquez@cobornsinc.com>
2022-01-20 13:12:20 -0600

    shopOnline

--- src/data/mainNavigation.js
+++ src/data/mainNavigation.js
@@ -5,10 +5,23 @@ const mainNavigation = [
     name: 'Shop Online',
     href: '#link',
     children: [
-      { name: 'Baby', href: '#link' },
-      { name: 'Bakery', href: '#link' },
-      { name: 'Dairy', href: '#link' },
-      { name: 'Deli', href: '#link' }
+      { name: 'Baby', href: axios.get('#http://localhost:8009/grocery/tree/5/PRODUCTS/34') },
+      { name: 'Bakery', href: axios.get('#http://localhost:8009/grocery/tree/5/PRODUCTS/109792') },
+      { name: 'Beer', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/' },
+      { name: 'Meat & Seafood', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Dairy', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Deli', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Floral', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Frozen', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'General Merchandise', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Grocery', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Health & Beauty', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Household & Laundry', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Liquor', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Pet', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Produce', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Tobacco', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
+      { name: 'Wine', href: '#http://localhost:8009/grocery/tree/5/PRODUCTS/109792' },
     ]
   },
   {
