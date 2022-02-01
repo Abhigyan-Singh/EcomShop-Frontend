6f21f97d580c057124265f396dc934fdaecfbe3b
Naadir Vasquez <naadir.vasquez@cobornsinc.com>
2022-02-01 13:13:09 -0600

    StoreLocator

--- src/services/groceryTree.js
+++ src/services/groceryTree.js
@@ -1,6 +1,6 @@
 import { config, API } from 'apiConfig';
 import apiClient from './api';
 
-export const grocery = (id) => {
-  return apiClient.get('http://localhost:8009/grocery/tree/5/PRODUCTS/4433');
+export const grocery = () => {
+  return apiClient.get('http://mp2-dev-web2.cobornsinc.local:8009/grocery/tree/5/PRODUCTS/4433');
 };
