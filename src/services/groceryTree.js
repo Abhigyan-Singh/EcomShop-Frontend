585b61b9dd773859fcd714e547b7d0535cca8548
Naadir Vasquez <Naadir.Vasquez@cobornsinc.com>
2022-02-01 04:55:45 +0000

    Updated groceryTree.js

--- src/services/groceryTree.js
+++ src/services/groceryTree.js
@@ -2,5 +2,5 @@ import { config, API } from 'apiConfig';
 import apiClient from './api';
 
 export const grocery = (id) => {
-  return apiClient.get('http://localhost:8009/grocery/tree/5/PRODUCTS/109794');
+  return apiClient.get('http://localhost:8009/grocery/tree/5/PRODUCTS/4433');
 };
