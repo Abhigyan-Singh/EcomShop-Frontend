0da1947493366b9fdc48e4b900151c9154b6d5f9
Naadir Vasquez <naadir.vasquez@cobornsinc.com>
2022-02-04 11:33:59 -0600

    crossapp

---
+++ src/services/crossApp.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
+import config  from '../environments/config'
+
+
+
+
+const apiClient2 = axios.create({
+    baseURL: config.url,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': config,
+    }
+  });
+  
+  apiClient2.interceptors.request.use(
+    (config) => {
+      const cookies = new Cookies();
+      const jwt = cookies.get('user');
+      if (jwt) {
+        config.headers['Authorization'] = 'Bearer ' + jwt.token;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+
+
+export const legacy = () => {
+  return apiClient2.post('https://shop.coborns.com/myaccountdetails'); 
+};
+
