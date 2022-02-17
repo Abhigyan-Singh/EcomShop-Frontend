f6d76ddf54fb5667b026826a5e9d4e0971251a8b
paleti <pranay.aleti@cobornsinc.com>
2022-02-17 13:24:00 -0600

    menu bar fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import { Cookies } from 'react-cookie';
-import config  from '../environments/config'
-
+import config from '../environments/config';
 
 const apiClient = axios.create({
   baseURL: config.url,
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json'
   }
 });
 
