ded7a771170e56826632aaab10d1b4731460c46c
Pranay Aleti <paleti@cobornsinc.local>
2022-04-04 12:18:25 -0600

    dropdowns

--- src/composites/shop-get.js
+++ src/composites/shop-get.js
@@ -1,89 +1,19 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
-import useFetch from '../hooks/useFetch';
-import queryString from 'query-string';
+import React, { useState, useRef, useEffect } from 'react';
 import Item from 'components/item/item';
 import { getAllList } from 'services/mylist';
-import { CartState, Context } from 'context/context';
-import { useCookies } from 'react-cookie';
-import { CookiesAge } from 'apiConfig';
-import useCart from 'services/addtocart';
-import { useDeleteFavorite } from 'services/favorites';
-import { userInfoService } from 'services/auth';
+import { Context } from 'context/context';
 
-const ShopGetPage = () => {
-  const params = window.location.href.split('?')[1];
-  const { text: searchText } = queryString.parse(params);
-  const [query, setQuery] = useState(searchText);
-  const [page, setPage] = useState(1);
-  const { loading, error, list } = useFetch(query, page);
+const ShopGetPage = ({ loader, list, error, loading }) => {
   const [listItems, setListItems] = useState([]);
-  const loader = useRef(null);
-  const [cookies, setCookie] = useCookies(['user']);
-  const { userInfo } = cookies;
-  const { dispatchUser } = CartState();
-  const { fetchFavorites } = useDeleteFavorite();
-
-  const { getCartDetails } = useCart();
-
-  useEffect(() => {
-    if (!userInfo) {
-      userInfoService().then((userRes) => {
-        if (userRes.data) {
-          setCookie('userInfo', userRes.data, {
-            path: '/',
-            maxAge: CookiesAge
-          });
-          dispatchUser({
-            type: 'SET_USER',
-            payload: { userName: userRes.data.userName }
-          });
-          getCartDetails(userRes.data.userName);
-        }
-      });
-    }
-  }, [userInfo]);
-
-  useEffect(() => {
-    getCartDetails();
-    fetchFavorites();
-  }, []);
 
   const getListItems = async () => {
     const res = await getAllList();
     setListItems(res.data);
   };
 
-  const handleChange = (e) => {
-    setQuery(e?.target?.value);
-  };
   useEffect(() => {
-    handleChange();
     getListItems();
-    console.log('LIST', list);
   }, []);
-
-  const handleObserver = useCallback((entries) => {
-    const target = entries[0];
-    if (target.isIntersecting) {
-      setPage((prev) => prev + 1);
-    }
-  }, []);
-
-  useEffect(() => {
-    const option = {
-      root: null,
-      rootMargin: '20px',
-      threshold: 0
-    };
-    const observer = new IntersectionObserver(handleObserver, option);
-    if (loader.current) observer.observe(loader.current);
-  }, [handleObserver]);
-
-  const filter = () => {
-    list
-      .filter((item) => item.brand === "Amy's")
-      .map((filterdItems) => <Item item={filterdItems}></Item>);
-  };
 
   return (
     <div className="App">
