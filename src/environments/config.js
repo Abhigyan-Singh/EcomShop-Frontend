a00eb1f8ad7f591c41a0839bbb58848225698798
Bob Jendro <Bob.Jendro@cobornsinc.com>
2022-06-02 20:31:14 +0000

    Updated config.js - added test

--- src/environments/config.js
+++ src/environments/config.js
@@ -1,5 +1,6 @@
 import { local } from './environment';
 import { dev} from './environment.dev';
+import { test } from './environment.test';
 import { prod } from './environment.prod';
 import { stage } from './environment.stage';
 
@@ -8,6 +9,8 @@ if (process.env.REACT_APP_ENV === 'dev') {
     config = dev;
 } else if (process.env.REACT_APP_ENV === 'stage') {
     config = stage;
+} else if (process.env.REACT_APP_ENV === 'test') {
+    config = test;
 } else if (process.env.REACT_APP_ENV === 'prod') {
     config = prod;
 }
