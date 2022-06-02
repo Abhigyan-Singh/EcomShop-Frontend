d3832755e514bf6bc8f2313817c1993734f8593b
skyatham <shilaja.kyatham@cobornsinc.com>
2022-06-02 15:45:31 -0500

    Adding test properties

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -188,6 +188,7 @@ const Header = (props) => {
     const urlObj = {
       localhost: 'https://devweb2.shop.coborns.com',
       dev: 'https://devweb.shop.coborns.com',
+      test: 'https://tshop.coborns.com',
       prod: 'https://shop.coborns.com'
     };
     const path = '/checkautomaticpromotions';
@@ -197,7 +198,10 @@ const Header = (props) => {
       url = urlObj['localhost'];
     } else if (host.includes('devweb2.shop.coborns.com')) {
       url = urlObj['dev'];
-    } else if (host.includes('shop.coborns.com')) {
+    }  else if (host.includes('tshop.coborns.com')) {
+      url = urlObj['test'];
+    }
+    else if (host.includes('shop.coborns.com')) {
       url = urlObj['prod'];
     } else {
       url = urlObj['localhost'];
@@ -395,7 +399,7 @@ const Header = (props) => {
               {!user && (
                 <a
                   className="underline"
-                  href="https://devweb2.shop.coborns.com/createaccount"
+                  href="https://tshop.coborns.com/osl/createaccount"
                 >
                   Register
                 </a>
