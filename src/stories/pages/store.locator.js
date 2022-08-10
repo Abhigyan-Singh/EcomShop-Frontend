af1d84640991df2af2629941be8f0a2bb361c54e
Naadir Vasquez <naadir.vasquez@cobornsinc.com>
2022-08-10 15:47:00 -0500

    locator

--- src/stories/pages/store.locator.js
+++ src/stories/pages/store.locator.js
@@ -2,7 +2,7 @@ import { Fragment, useState, useEffect, useRef } from 'react';
 import { useCookies } from 'react-cookie';
 import { CookiesAge } from 'apiConfig';
 import { allStores } from 'services/facilities';
-import { map } from 'lodash';
+//import { map } from 'lodash';
 import PropTypes from 'prop-types';
 import { Transition, Popover, Dialog} from '@headlessui/react';
 import Radio from 'components/radio/radio';
@@ -11,9 +11,13 @@ import { useNavigate } from 'react-router-dom';
 import L from 'leaflet'
 import qs from 'qs'
 import axios from 'axios'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-import { OpenStreetMapProvider, GeoSearchControl } from 'leaflet-geosearch'
-import { searchFacilities } from 'services/store.locator.facilities';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { searchFacilities } from 'services/store.locator.facilities'
 
 export default {
   title: 'Pages/Home',
@@ -31,8 +35,8 @@ export default {
 
 export const StoreLocator = (props) => {
   const { className, onFacilityChange} = props
-  const [store, setStore] = useState();
-  const [storeDelivery, setStoreDelivery] = useState();
+  const [store, setStore] = useState([]);
+  const [storeDelivery, setStoreDelivery] = useState([]);
   const [cookies, setCookie] = useCookies();
   const { facility, dept, user, userInfo } = cookies;
   const [selected, setSelected] = useState(facility);
@@ -66,9 +70,9 @@ export const StoreLocator = (props) => {
   useEffect(() => {
     searchFacilities(2031).then((res) => {
       setStore(res.data);
-      console.log("data",store)
+      console.log("NEW", res.data)
     });
-  }, [store]);
+  }, []);
 
   useEffect(() => {
     allStores(7).then((res) => {
@@ -108,7 +112,6 @@ export const StoreLocator = (props) => {
       document.removeEventListener("mousedown", handleClickOutside)
     }
   })
-
 
   const handleFacilityChange = (option) => {
     setSelected(option);
@@ -151,279 +154,84 @@ export const StoreLocator = (props) => {
     }
   }, )
 
+  useEffect(() => {
+    let map = L.map('map', {
+      center: [45.23389900, -93.66082100],
+      zoom: 10
+    });
 
-  //'map' refers to a <div> element with the ID map
-  const map = () => {
-    window.onload = function() {
-      console.log("STARTED")
-      L.mapquest.key = 'Gmjtd|luu2206zn9,8g=o5-lz2s1';
-      var map = L.mapquest.map('map', {
-        center: [45.23389900, -93.66082100],
-        layers: L.mapquest.tileLayer('map'),
-        zoom: 7
-      });
-      map.addControl(L.mapquest.control());
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+  }, [])
+  
+    //'map' refers to a <div> element with the ID map
+    // L.mapquest.key = 'Gmjtd|luu2206zn9,8g=o5-lz2s1';
+    // L.mapquest.open = true;
+    // L.mapquest.map('map', {
+    //   center: [45.23389900, -93.66082100],
+    //   layers: L.mapquest.tileLayer('map'),
+    //   zoom: 12
+    // });
+
+  // const List = () => {
+  //   return (
+  //     <div>
+  //       <ul>
+  //         <li>
+  //         {store &&
+  //           map(store, (option, index) => (
+  //           <Card style={{ maxWidth: "25%", borderBottomWidth: 1, borderBottomColor: 'black' }}>
+  //             <CardContent>
+  //               <Typography variant="h5" component="div">
+  //                 {option.facilityName}
+  //               </Typography>
+  //               <Typography variant="body2" style={{ paddingTop:20 }}>
+  //                 {option.address1}, {option.state} {option.zipCode}
+  //               </Typography>
+  //               <Typography variant="body2" style={{ paddingTop:20 }}>
+  //                 Store: {option.phoneNumber}
+  //               </Typography>
+  //               <Typography variant="body2">
+  //                 Customer Relations : {option.customerRelationsPhone}
+  //               </Typography>
+  //               <Typography variant="body2" style={{ paddingTop:20 }}>
+  //                 <div id="yext-facility-hours-getter" />
+  //               </Typography>
+  //             </CardContent>
+  //             <CardActions>
+  //               <Button size="small">Shop This Store</Button>
+  //             </CardActions>
+  //           </Card>
+  //         ))}
+  //         </li>
+  //       </ul>
+  //     </div>
+  //   )
+  // }
+
+  const setHoursHtml = () => {
+    if (
+      document.getElementById('yext-facility-hours-getter') &&
+      document.getElementById('yext-facility-hours-setter')
+    ) {
+      document.getElementById('yext-facility-hours-getter').innerHTML =
+      document.getElementById('yext-facility-hours-setter').innerHTML;
     }
-  }
+  };
 
-  const Search = () => {
-    const map = useMap();
-    useEffect(() => {
-      const provider = new OpenStreetMapProvider();
-      const searchControl = new GeoSearchControl({
-        provider,
-        style: 'button',
-        marker: {
-          //icon
-        }
-      });
-      map.addControl(searchControl);
-      return () => map.removeControl(searchControl);
-    }, []);
-    return null;
-  }
+  useEffect(() => {
+    setHoursHtml()
+  }, [])
 
   const position = [45.23389900, -93.66082100]  
   return (
     <div>
       <Locator preStore={selectedFacility} />
-      <div>  
-        <div>
-          <MapContainer  style={{ height: 300, width: '33%'}} center={position} zoom={10} scrollWheelZoom={true}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
+      <input style={{height: 50, width:"25%"}}></input>
+      <div id="map" style={{width: 500, height: 500}}></div>
       
-            </Marker>
-    
-          </MapContainer> 
-        </div>
-        <Popover>
-          {({ open }) => (
-            <div
-              href="#"
-              style={{
-                color: '#9ac035',
-                fontSize: 20,
-                paddingBottom: 10,
-                paddingTop: 10,
-                paddingLeft: 20,
-                paddingRight: 9
-              }}
-              className="flex"
-            > Preferred Store
-              <Popover.Button
-                onMouseEnter={() => onHover(open, "onMouseEnter")}
-                onMouseLeave={() => onHover(open, "onMouseLeave")}
-                ref={buttonRef}  
-                type="button"
-                onClick={() => handleClick(open)}
-                style={{marginLeft: 15,paddingTop: 5}}
-                className="bg-white text-sm font-medium text-black hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-              >
-                What does preferred store mean? 
-              </Popover.Button>
-              <Transition
-                show={open}
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-1"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 translate-y-1"
-              >
-                <Popover.Panel  
-                  static
-                  className="relative -ml-0 mt-1"
-                  style={{ zIndex: 9999 }}>
-                  <p
-                    class="overflow-auto absolute border shadow-lg p-4 bg-white rounded justify-center"
-                    style={{
-                      backgroundColor: 'rgb(255, 255, 255, 1)',
-                      margin: -3,
-                      color: 'black',
-                      width: 800,
-                      height: 155
-                    }}>
-                    What does preferred store mean? Your preferred store
-                    is intended to be the store you shop more frequently.
-                    Your preferred store is selected during account sign
-                    up or may be changed anytime in My Account. You may
-                    shop stores other than your preferred store at any time.
-                  </p>
-                </Popover.Panel>
-              </Transition>
-            </div>
-          )}
-        </Popover>
-        </div>
-          <div>
-            <div style={{ paddingLeft: 20, paddingBottom: 10 }}>
-              DELIVERY SERVICE
-            </div>
-            {storeDelivery &&
-              map(storeDelivery.facilitiesDeliveryOrPickup, (option, index) => (
-                <Radio
-                  className="mb-4"
-                  id="checkbox-1"
-                  key={option.facilityDtl.facilityName}
-                  label={option.facilityDtl.facilityName}
-                  style={{ paddingBottom: 10, marginLeft: 50, paddingTop: 5 }}
-                  checked={option.facilityDtl.facilityName == selectedFacility}
-                  onChange={() => onChange(option)}
-                />
-              ))}
-          </div>
-          <div>
-            <div style={{ paddingLeft: 20, paddingBottom: 10 }}>
-              STORE PICK UP SERVICE
-            </div>
-            {store &&
-              map(store, (option, index) => (
-                <Radio
-                  className="mb-4"
-                  id="checkbox-1"
-                  key={option.facilityName}
-                  label={option.facilityName}
-                  style={{ paddingBottom: 10, marginLeft: 50, fontWeight: 'bold'}}
-                  checked={option.facilityName == selectedFacility}
-                  onChange={() => onChange(option)}
-                />
-              ))}
-          </div>
-          <div >
-          <>
-            <div style={{paddingBottom: 20, paddingLeft: 10 }} className="flex justify-left">  
-            <button
-                type="button"
-                onClick={() => {
-                  navigate('/')
-                  window.scrollTo(0, 0)
-                  setIsOpen(false)
-                }}
-                style={{
-                  width: 190,
-                  height: 40,
-                  borderRadius: 7, 
-                  backgroundColor: '#9ac035',
-                  borderColor: '#9ac035',
-                  borderWidth: 3,
-                  fontSize: 20,
-                  marginLeft: 10
-                }}>Cancel
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  handleSaveClick()
-                }}
-                style={{
-                  width: 190,
-                  height: 40,
-                  borderRadius: 7, 
-                  backgroundColor: '#9ac035',
-                  borderColor: '#9ac035',
-                  borderWidth: 3,
-                  fontSize: 20,
-                  marginLeft: 30
-                }}>Save
-              </button>
-            </div>
-            <Transition.Root appear show={isOpen} as={Fragment}>
-              <Dialog
-                as="div" 
-                className="relative z-10" 
-                onClose={() => setIsOpen(false)}>
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
-                </Transition.Child>
-                <div className="fixed inset-0 overflow-y-auto">
-                  <div className="flex min-h-full items-center justify-center p-4 text-center">
-                    <Transition.Child
-                      as={Fragment}
-                      enter="ease-out duration-300"
-                      enterFrom="opacity-0 scale-95"
-                      enterTo="opacity-100 scale-100"
-                      leave="ease-in duration-200"
-                      leaveFrom="opacity-100 scale-100"
-                      leaveTo="opacity-0 scale-95"
-                    >
-                      <Dialog.Overlay className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                        <Dialog.Title
-                            as="h3"
-                            className="text-lg font-medium leading-6 text-gray-900"
-                          >
-                            Save Successful
-                        </Dialog.Title>
-                        <div>
-                            <p className="text-sm text-gray-500">
-                                Your account change has been saved.
-                                Do you want to navigate to your new preferred store now?
-                            </p>
-                          </div>
-                          <div
-                             style={{
-                              display: 'flex',
-                              alignItems: 'center',
-                              justifyContent: 'center',
-                            }} >
-                            <button
-                            onClick={() => {
-                              setIsOpen(false)
-                              window.scrollTo(0, 0)
-                            }}
-                            style={{
-                            width: 190,
-                            height: 40,
-                            borderRadius: 7, 
-                            backgroundColor: '#9ac035',
-                            borderColor: '#9ac035',
-                            borderWidth: 3,
-                            fontSize: 20,
-                            marginTop: 20
-
-                            }}>
-                              NO
-                          </button>
-                          <button
-                            onClick={() => {
-                              handleYesClick(selected2)
-                              setIsOpen(false)
-                              navigate("/")
-                            }}
-                            style={{
-                              width: 190,
-                              height: 40,
-                              borderRadius: 7, 
-                              backgroundColor: '#9ac035',
-                              borderColor: '#9ac035',
-                              borderWidth: 3,
-                              fontSize: 20,
-                              marginLeft: 20,
-                              marginTop: 20
-                            }}>
-                              YES
-                            </button>
-                          </div>
-                      </Dialog.Overlay>
-                    </Transition.Child>
-                  </div>
-                </div>
-              </Dialog>
-            </Transition.Root>
-          </>
-        </div>
     </div>
   );
 };
